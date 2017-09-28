16769a6603dfe3b950758650c7735959087f1c0b
yangshuai <yangshuai@yangshuaideMacBook-Pro.local>
2017-09-28 15:44:05 +0800

    packaged createWindow

--- app/main.js
+++ app/main.js
@@ -3,9 +3,9 @@ const {app,BrowserWindow,ipcMain} = require('electron'),path = require('path'),u
 
 let win = {};    //声明窗口对象
 
-function createWindow (name) {
+function createWindow (name,windowStyle) {
     //创建浏览器窗口。
-    win[name] = new BrowserWindow({width:491,height:351,frame:false,resizable:false,show:false});
+    win[name] = new BrowserWindow(windowStyle);
     //防止可视闪烁
     win[name].once('ready-to-show', () => {win[name].show()});
     // 加载应用界面
@@ -22,7 +22,9 @@ function createWindow (name) {
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', () => {createWindow('login')});
+app.on('ready', () => {
+    createWindow('login', {width:491,height:351,frame:false,resizable:false,show:false});
+});
 
 app.on('window-all-closed', () => {app.quit()});    //当全部窗口关闭时退出。
 
