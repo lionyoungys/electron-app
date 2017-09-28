b0b0096bfd5f9ab9061e4947b6b557c119df7810
yangshuai <yangshuai@yangshuaideMacBook-Pro.local>
2017-09-28 08:00:26 +0800

    init project

--- app/main.js
+++ app/main.js
@@ -3,30 +3,19 @@ const path = require('path'),url = require('url');
 
 // 保持一个对于 window 对象的全局引用，如果你不这样做，
 // 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
-let win
+let win;
 
 function createWindow () {
-  // 创建浏览器窗口。
-  win = new BrowserWindow({width:491,height:351,frame:false,resizable:false,show:false})
-  win.once('ready-to-show', () => {win.show()})    //防止可视闪烁
+    // 创建浏览器窗口。
+    win = new BrowserWindow({width:491,height:351,frame:false,resizable:false,show:false})
+    win.once('ready-to-show', () => {win.show()})    //防止可视闪烁
 
-  // 加载应用的 index.html。
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, '/public/login.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // 打开开发者工具。
-  //win.webContents.openDevTools()
-
-  // 当 window 被关闭，这个事件会被触发。
-  win.on('closed', () => {
-    // 取消引用 window 对象，如果你的应用支持多窗口的话，
-    // 通常会把多个 window 对象存放在一个数组里面，
-    // 与此同时，你应该删除相应的元素。
-    win = null;
-  })
+    // 加载应用的 index.html。
+    win.loadURL(url.format({pathname: path.join(__dirname, '/public/login.html'),protocol: 'file:',slashes: true}))
+    // 打开开发者工具。
+    //win.webContents.openDevTools()
+    // 当 window 被关闭，这个事件会被触发。
+    win.on('closed', () => {win = null;})    //dangwindow关闭时取消引用
 }
 
 // Electron 会在初始化后并准备
@@ -35,21 +24,25 @@ function createWindow () {
 app.on('ready', createWindow)
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // 在这文件，你可以续写应用剩下主进程代码。
-  // 也可以拆分成几个文件，然后用 require 导入。
-  if (win === null) {
-    createWindow()
-  }
+    // 在这文件，你可以续写应用剩下主进程代码。
+    // 也可以拆分成几个文件，然后用 require 导入。
+    if (win === null) {
+        createWindow()
+    }
 })
 
 // 在这文件，你可以续写应用剩下主进程代码。
 // 也可以拆分成几个文件，然后用 require 导入。
-ipcMain.on('close-login',() => {win.close();});    //登录界面关闭监听
+ipcMain.on('login-msg',(e,args) => {    //登录界面ipc监听
+    if ('close' === args) {
+        win.close();
+    }
+});
