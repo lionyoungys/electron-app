a53f2639e6be58de317e643eb71da1f16d3021ea
yangshuaiWithCompany <go.xlv@qq.com>
2017-10-28 18:19:16 +0800

    新增员工管理界面

--- src/manage/clerk_manage.js
+++ src/manage/clerk_manage.js
@@ -11,17 +11,23 @@ export default class ClerkManage extends Component {
     constructor(props) {
         super(props);
         this.crumbs = [{key:0,text:'员工管理'}];
-        this.state = {clerks:[],show:false};
+        this.state = {clerks:[],show:false,authList:[]};
         this.callback = this.callback.bind(this);    //弹窗回调函数
         this.deleteClerk = this.deleteClerk.bind(this);    //删除员工
     }
 
     componentDidMount() {
+        axios.post(api.U('getAuth'),api.data({token:this.props.token}))
+        .then(response => {
+            for (var k in response.data) {this.state.authList.push(response.data[k])}
+            this.setState({authList:this.state.authList});
+            console.log(this.state.authList);
+        });
         axios.post(api.U('clerkList'),api.data({token:this.props.token}))
         .then((response) => {
             let result = response.data.data;
             this.setState({clerks:result});
-            console.log(result);
+            //console.log(result);
         });
     }
 
@@ -92,7 +98,7 @@ export default class ClerkManage extends Component {
                         </table>
                     </div>
                 </section>
-                <AddClerk show={state.show} callback={this.callback} token={props.token}/>
+                <AddClerk show={state.show} authList={state.authList} callback={this.callback} token={props.token}/>
             </div>
         );
     }
@@ -109,7 +115,9 @@ class AddClerk extends Component {
         this.timeOutId = this.intervalId = null;
         this.onConfirm = this.onConfirm.bind(this);    //确认
         this.captcha = this.captcha.bind(this);    //获取验证码
+        this.toggleChecked = this.toggleChecked.bind(this);
     }
+
 
     componentWillUnmount() {
         if (null !== this.timeOutId) {clearTimeout(this.timeOutId);}
@@ -119,7 +127,7 @@ class AddClerk extends Component {
     onConfirm() {
         let state = this.state,
             props = this.props;
-        if (state.captcha !== state.captchaInfo) {
+        if (md5(state.captcha) !== state.captchaInfo) {
             this.setState({show:true});
             this.timeOutId = setTimeout(() => {this.setState({show:false})}, 3000);
         } else {
@@ -145,7 +153,7 @@ class AddClerk extends Component {
             props = this.props;
         if ('获取验证码' != state.msg) return;
         if (state.mobile.length === 11) {
-            axios.post(api.U('addClerk'),api.data({token:props.token,mobile:state.mobile}))
+            axios.post(api.U('clerkSendCode'),api.data({token:props.token,mobile:state.mobile}))
             .then((response) => {
                 if (api.verify(response.data)) {
                     this.setState({captchaInfo:response.data.data,msg:'60s'});
@@ -166,57 +174,77 @@ class AddClerk extends Component {
             });
         }
     }
+
+    toggleChecked(e) {
+        let target = e.target;
+        if (target.classList.contains('ui-checked3')) {
+
+        }
+        target.classList.toggle('ui-checked3');
+    }
     
     render () {
         let props = this.props,
-            state = this.state;
+            state = this.state,
+            auths = props.authList.map((obj, index) => 
+                <span 
+                    key={index} 
+                    data-index={index + 1} 
+                    className='ui-checkbox3' 
+                    onClick={this.toggleChecked}
+                >{obj}</span>  
+            );
         if (!props.show) return null;
         return (
-            <section className='ui-alert-bg'>
+            <section className='ui-fixed-bg'>
                 <Notification show={state.show} width='120'>验证码不正确!</Notification>
                 <div className='ui-clerk-box'>
-                    <div className='ui-clerk-box-up'>
-                        <div className='ui-clerk-box-title'>新增员工</div>
-                        <div className='ui-clerk-box-content'>
-                            <p>
-                                <span>姓名：</span>
-                                <input 
-                                    type='text' 
-                                    className='ui-clerk-box-input' 
-                                    maxLength='4'
-                                    onChange={e => this.setState({name:e.target.value})}
-                                />
-                            </p>
-                            <p>
-                                <span>手机号：</span>
-                                <input 
-                                    type='text' 
-                                    className='ui-clerk-box-input' 
-                                    onChange={e => this.setState({mobile:e.target.value})}
-                                />
-                            </p>
-                            <p>
-                                <span>验证码：</span>
-                                <input 
-                                    type='text' 
-                                    className='ui-clerk-box-input2'
-                                    onChange={e => this.setState({captcha:md5(e.target.value)})}
-                                />
-                                <span onClick={this.captcha}>{state.msg}</span>
-                            </p>
-                            <p>
-                                <span>密码：</span>
-                                <input 
-                                    type='password' 
-                                    className='ui-clerk-box-input'
-                                    onChange={e => this.setState({password:e.target.value})}
-                                />
-                            </p>
-                        </div>
+                    <div className='ui-clerk-box-header'>
+                        <span>添加员工</span>
+                        <em className='ui-close3' onClick={() => props.callback(false)}></em>
                     </div>
-                    <div className='ui-clerk-box-down'>
-                        <div className='ui-clerk-box-btn' onClick={() => props.callback(false)}>取消</div>
-                        <div className='ui-clerk-box-btn' onClick={this.onConfirm}>确认</div>
+                    <div className='ui-clerk-box-body'>
+                        <div className='ui-clerk-box-row'>
+                            <span>姓名：</span>
+                            <input 
+                                type='text' 
+                                value={state.name} 
+                                onChange={e => this.setState({name:e.target.value})}
+                            />
+                        </div>
+                        <div className='ui-clerk-box-row'>
+                            <span>手机号：</span>
+                            <input 
+                                type='text' 
+                                value={state.mobile}
+                                onChange={e =>{if (!isNaN(e.target.value)) this.setState({mobile:e.target.value})}}
+                            />
+                        </div>
+                        <div className='ui-clerk-box-row'>
+                            <span>验证码：</span>
+                            <input 
+                                type='text' 
+                                className='input' 
+                                value={state.captcha}
+                                onChange={e => this.setState({captcha:e.target.value})}
+                            />
+                            <span className='captcha' onClick={this.captcha}>{state.msg}</span>
+                        </div>
+                        <div className='ui-clerk-box-row'>
+                            <span>密码：</span>
+                            <input 
+                                type='text' 
+                                value={state.password} 
+                                onChange={e => this.setState({password:e.target.value})}
+                            />
+                        </div>
+                        <div className='ui-clerk-box-row' style={{paddingTop:'16px'}}>
+                            <span style={{height:'24px',lineHeight:'24px'}}>权限设置：</span>
+                            <div className='auth'>{auths}</div>
+                        </div>
+                        <div className='ui-clerk-box-row' style={{marginTop:'-6px'}}>
+                            <span></span><input type='button' className='ui-teamwork-confirm' onClick={this.onConfirm}/>
+                        </div>
                     </div>
                 </div>
             </section>
