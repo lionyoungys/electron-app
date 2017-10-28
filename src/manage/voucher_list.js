5c2f62e806e2fe3f605765de688378645eb70201
yangshuaiWithCompany <go.xlv@qq.com>
2017-10-28 14:29:13 +0800

    新增clipboard.js及使用

--- src/manage/voucher_list.js
+++ src/manage/voucher_list.js
@@ -9,8 +9,11 @@ import Crumbs from '../static/UI';
 export default class VoucherList extends Component{
     constructor(props) {super(props);}
 
+    componentDidMount() {new Clipboard(this.btn);}
+
     render() {
-        let props = this.props;
+        let props = this.props,
+            clipboardText = '385729847392830564\n385729847392830564\n385729847392830564\n';
         return (
             <div>
                 <Crumbs crumbs={[{key:0,text:'制作代金券',e:'voucher'},{key:1,text:'生成代金券'}]} callback={props.changeView}/>
@@ -36,7 +39,15 @@ export default class VoucherList extends Component{
                         <div className='row'>385729847392830564</div>
                         <div className='row'>385729847392830564</div>
                         <div className='row'>385729847392830564</div>
-                        <div className='row'><input type='button' value='一键复制' className='ui-btn ui-btn-confirm'/></div>
+                        <div className='row'>
+                            <input 
+                                type='button' 
+                                value='一键复制' 
+                                className='ui-btn ui-btn-confirm'
+                                data-clipboard-text={clipboardText}
+                                ref={btn => this.btn = btn}
+                            />
+                        </div>
                     </div>
                 </section>
             </div>
