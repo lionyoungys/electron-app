b73630e446a9c0797dd033f26749601d4a912f3e
yangshuaiWithCompany <go.xlv@qq.com>
2018-01-31 11:16:54 +0800

    新增合作门店

--- src/Module/teamwork/App.js
+++ src/Module/teamwork/App.js
@@ -11,14 +11,17 @@ import './App.css';
 export default class extends Component {
     constructor(props) {
         super(props);
-        this.state = {data:[]};
+        this.state = {data:[],show:false};
+        this.query = this.query.bind(this);
     }
 
     componentDidMount() {
+        this.query();
+    }
+    query() {
         axios.post(api.U('teamwork'), api.D({token:this.props.token}))
         .then(response => {
-            console.log(response.data);
-            api.V(response.data) && this.setState({data:response.data.result});
+            api.V(response.data) && this.setState({data:response.data.result,show:false});
         });
     }
 
@@ -36,8 +39,8 @@ export default class extends Component {
                 <Crumb data={[{key:0,value:'合作门店'}]} callback={this.props.changeView}/>
                 <div className='m-container'>
                     <div className='m-text-r' style={{position:'relative'}}>
-                        <button className='m-btn confirm middle' type='button'>+ 新增合作门店</button>
-                        <Query/>
+                        <button className='m-btn confirm middle' type='button' onClick={() => this.setState({show:true})}>+ 新增合作门店</button>
+                        <Query token={this.props.token} callback={this.query} show={this.state.show} onClose={() => this.setState({show:false})}/>
                     </div>
                     <div className='m-box'>
                         <table className='m-table m-text-c'>
@@ -54,19 +57,54 @@ export default class extends Component {
 class Query extends Component {
     constructor(props) {
         super(props);
-        this.state = {value:''}
+        this.state = {value:'',data:[],checked:null}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.callback = this.callback.bind(this);
+    }
+
+    handleChange(e) {
+        let value = e.target.value;
+        this.setState({value:value,checked:null});
+        if (value.length < 1) return;
+        axios.post(api.U('merchant_search'),api.D({token:this.props.token,keywords:value}))
+        .then(response => {
+            api.V(response.data) && this.setState({data:response.data.result});
+            console.log(response.data.result);
+        });
+    }
+    handleClick(e) {
+        this.setState({value:this.state.data[e.target.dataset.index].mname,checked:e.target.dataset.id});
+    }
+    callback() {
+        if (null !== this.state.checked) {
+            axios.post(api.U('teamwork_add'),api.D({token:this.props.token,ids:this.state.checked}))
+            .then(response => {
+                if (api.V(response.data)) {
+                    this.props.callback();
+                } else {
+                    alert(response.data.msg);
+                }
+            });
+        }
     }
 
     render() {
+        if (!this.props.show) return null;
+        let html = this.state.data.map( (obj, index) => 
+            <div key={obj.id} data-id={obj.id} data-index={index} onClick={this.handleClick}>{obj.mname}&nbsp;&nbsp;{obj.id}</div>
+        );
         return (
             <div className='teamwork-query'>
-                <i className="fa fa-times"></i>
+                <i className="fa fa-times" onClick={this.props.onClose}></i>
                 <div>新增合作门店</div>
                 <div className='body'>
                     <div>
-                        <input type='text' value={this.state.value} onChange={e => this.setState({value:e.target.value})}/>
-                        <button className='m-btn confirm' type='button'>确定</button>
-                        <div className='query-result'></div>
+                        <input type='text' placeholder='请输入合作店铺名称/编号' value={this.state.value} onChange={this.handleChange}/>
+                        <button className='m-btn confirm' type='button' onClick={this.callback}>确定</button>
+                        <div className='query-result' style={{display:(this.state.data.length > 0 ? 'block' : 'none')}}>
+                            {html}
+                        </div>
                     </div>
                 </div>
             </div>
