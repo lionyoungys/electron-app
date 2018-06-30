315e5cb4eef20b79f1ceaaacae4d2f3734836db6
EdwinYoung <go.xlv@qq.com>
2018-06-30 21:39:35 +0800

    界面修改

--- src/Module/offline_add_item/App.js
+++ src/Module/offline_add_item/App.js
@@ -83,6 +83,7 @@ export default class extends React.Component {
         item.clean_sn = '';
         this.state.data.push(item);
         this.setState({data:this.state.data});
+        this.reloadAmount();
     }
     handleDeleteClick(dom, index) {
         if (tool.isSet(dom)) {
